cb39086bcef218cdaf16bb867b0051c69fc42735
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 15:06:02 +0300

    scroll indicator created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ScrollIndicator from "./components/scroll-indicator";
+
+function App() {
+    return (
+        <div className="App">
+            {/* Scroll indicator component */}
+            <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/>
+        </div>
+    );
+}
+
+export default App;
